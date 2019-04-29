27cbdb77b539e6097d921488919c6a5f9c0b6e97
Ricartho Franck <ricartho.franck@esih.edu>
2019-04-29 15:44:49 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,14 +1,3 @@
 module Memorable
-  @@artists  = []
- def initialize
-    @@artists << self
-    @songs = []
-  end
-  def reset_all
-    all.clear
-  end
 
-  def count
-    @@artists.count
-  end
 end 
